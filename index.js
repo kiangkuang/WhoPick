1a3db6f0805e2f6d2ea7ee7a597e5606c3c5e550
Kiang Kuang <kiangkuang@gmail.com>
2016-11-03 03:42:37 +0800

    add webhook

--- index.js
+++ index.js
@@ -3,7 +3,16 @@
 const Telegram = require('telegram-node-bot')
 const TelegramBaseController = Telegram.TelegramBaseController
 const TextCommand = Telegram.TextCommand
-const tg = new Telegram.Telegram(process.env.BOT_TOKEN)
+
+const tg = process.env.NODE_ENV === 'production' ?
+    new Telegram.Telegram(process.env.BOT_TOKEN, {
+        webhook: {
+            url: process.env.HEROKU_URL + '/' + process.env.BOT_TOKEN,
+            port: process.env.PORT,
+            host: 'localhost'
+        }
+    }) : 
+    new Telegram.Telegram(process.env.BOT_TOKEN)
 
 class PingController extends TelegramBaseController {
     /**
