d4c4537b953e8d10d49ae72c83f64d9a73405d66
Kiang Kuang <kiangkuang@gmail.com>
2016-11-03 02:12:48 +0800

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const Telegram = require('telegram-node-bot')
+const TelegramBaseController = Telegram.TelegramBaseController
+const TextCommand = Telegram.TextCommand
+const tg = new Telegram.Telegram(process.env.BOT_TOKEN)
+
+class PingController extends TelegramBaseController {
+    /**
+     * @param {Scope} $
+     */
+    pingHandler($) {
+        $.sendMessage('pong')
+    }
+
+    get routes() {
+        return {
+            'pingCommand': 'pingHandler'
+        }
+    }
+}
+
+tg.router
+    .when(
+        new TextCommand('ping', 'pingCommand'),
+        new PingController()
+    )
