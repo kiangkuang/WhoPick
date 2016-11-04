ec4527040b5d874201d307d96e5ee580ccbf8d88
Kiang Kuang <kiangkuang@gmail.com>
2016-11-05 03:55:09 +0800

    try 0.0.0.0 host

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const tg = process.env.NODE_ENV === 'production' ?
         webhook: {
             url: 'https://whopick.herokuapp.com',
             port: process.env.PORT,
-            host: 'localhost'
+            host: '0.0.0.0'
         }
     }) : 
     new Telegram.Telegram(process.env.BOT_TOKEN)
