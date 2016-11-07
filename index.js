c65b26ebede1b2e5880d76f9252b7510a1af1a1d
Kiang Kuang <kiangkuang@gmail.com>
2016-11-08 03:52:14 +0800

    inline query

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config({silent: process.env.NODE_ENV === 'production'})
+require('dotenv').config({silent: process.env.NODE_ENV === 'production'});
 if (!process.env.BOT_TOKEN || process.env.BOT_TOKEN === '<token>') {
-    console.log('ERROR: "BOT_TOKEN" env variable not set.')
-    return
+    console.log('ERROR: "BOT_TOKEN" env variable not set.');
+    return;
 }
 
 var TelegramBot = require('node-telegram-bot-api');
@@ -13,7 +13,7 @@ var bot = process.env.NODE_ENV === 'production' ?
     new TelegramBot(token, {polling: true});
 
 if (process.env.NODE_ENV === 'production') {
-    bot.setWebHook('https://whopick.herokuapp.com/bot' + token)
+    bot.setWebHook('https://whopick.herokuapp.com/bot' + token);
 }
 
 // Matches /echo [whatever]
@@ -22,61 +22,81 @@ bot.onText(/\/echo (.+)/, function(msg, match) {
     bot.sendMessage(msg.chat.id, resp);
 });
 
-var matched = false
+var matched = false;
 var userPolls = new Map();
 var wipPolls = new Map();
 
 
 // Matches /start
 bot.onText(/\/start/, function(msg, match) {
-    matched = true
+    matched = true;
     poll = {
         owner: msg.from.id,
         qn: '',
         ans: []
-    }
-    wipPolls.set(msg.from.id, poll)
+    };
+    wipPolls.set(msg.from.id, poll);
 
-    bot.sendMessage(msg.from.id, 'wat qn?')
+    bot.sendMessage(msg.from.id, 'wat qn?');
 });
 
 // Matches /done
 bot.onText(/\/done/, function(msg, match) {
-    matched = true
-    var wipPoll = wipPolls.get(msg.from.id)
+    matched = true;
+    var wipPoll = wipPolls.get(msg.from.id);
     if (wipPoll && wipPoll.qn && wipPoll.ans.length) {
-        wipPolls.delete(msg.from.id)
-        if (userPolls.has(msg.from.id)) {
-            userPolls.get(msg.from.id).push(wipPoll)
-        } else {
-            userPolls.set(msg.from.id, [wipPoll])
+        wipPolls.delete(msg.from.id);
+        if (!userPolls.has(msg.from.id)) {
+            userPolls.set(msg.from.id, new Map());
         }
-        
-        bot.sendMessage(msg.from.id, JSON.stringify(wipPoll))
-        console.log(userPolls)
+
+        userPolls.get(msg.from.id).set(msg.message_id, wipPoll);
+
+        bot.sendMessage(msg.from.id, JSON.stringify(wipPoll));
+        console.log(userPolls);
     } else {
-        bot.sendMessage(msg.from.id, 'wtf u trying to do')
+        bot.sendMessage(msg.from.id, 'wtf u trying to do');
     }
 });
 
 // Matches all other
-bot.onText(/(.*)/,function(msg, match) {
+bot.onText(/(.*)/, function(msg, match) {
     if (matched) {
-        matched = false
-        return
+        matched = false;
+        return;
     }
 
-    var wipPoll = wipPolls.get(msg.from.id)
+    var wipPoll = wipPolls.get(msg.from.id);
     if (wipPoll) {
         if (!wipPoll.qn) {
-            wipPoll.qn = match[0]
+            wipPoll.qn = match[0];
         } else {
-            wipPoll.ans.push(match[0])
+            wipPoll.ans.push(match[0]);
+        }
+
+        if (wipPollwipPoll.ans.length == 0) {
+            bot.sendMessage(msg.from.id, 'nice one lah. wat choice?');
+        } else {
+            bot.sendMessage(msg.from.id, 'nice one lah. wat other choice? /done if pang kang');
         }
     }
-    if (wipPoll.ans.length == 0) {
-        bot.sendMessage(msg.from.id, 'nice one lah. wat choice?')
-    } else {
-        bot.sendMessage(msg.from.id, 'nice one lah. wat other choice? /done if pang kang')
-    }
 });
+
+bot.on('inline_query', function(msg) {
+    var polls = userPolls.get(msg.from.id);
+    var results = [];
+    polls.forEach(function(poll, id) {
+        results.push({
+            type: 'article',
+            id: id.toString(),
+            message_text: 'blahblah',
+            title: poll.qn,
+            description: poll.ans.toString()
+        });
+    });
+
+    bot.answerInlineQuery(msg.id, results, {
+        switch_pm_text: 'new poll',
+        is_personal: true
+    });
+})
