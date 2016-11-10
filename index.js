383885e85f0d8e3de92c5bc94e50118054bcbf20
Kiang Kuang <kiangkuang@gmail.com>
2016-11-11 01:46:16 +0800

    add inline keyboard, poll output

--- index.js
+++ index.js
@@ -23,37 +23,63 @@ bot.onText(/\/echo (.+)/, function(msg, match) {
 });
 
 var matched = false;
-var userPolls = new Map();
-var wipPolls = new Map();
+var userPolls = new Map(); // polls of all users.       userPolls[userid][pollid] = poll
+var currPolls = new Map(); // currently being created.  currPolls[userid] = poll
 
 
 // Matches /start
 bot.onText(/\/start/, function(msg, match) {
     matched = true;
     poll = {
+        id: msg.message_id,
         owner: msg.from.id,
-        qn: '',
-        ans: []
+        question: '',
+        choices: []
     };
-    wipPolls.set(msg.from.id, poll);
+    currPolls.set(msg.from.id, poll);
 
-    bot.sendMessage(msg.from.id, 'wat qn?');
+    bot.sendMessage(msg.from.id, 'wat question?');
 });
 
 // Matches /done
 bot.onText(/\/done/, function(msg, match) {
     matched = true;
-    var wipPoll = wipPolls.get(msg.from.id);
-    if (wipPoll && wipPoll.qn && wipPoll.ans.length) {
-        wipPolls.delete(msg.from.id);
+    var currPoll = currPolls.get(msg.from.id);
+    if (currPoll && currPoll.question && currPoll.choices.length) { // currently creating a poll, have question, at least one choices
+        currPolls.delete(msg.from.id);
         if (!userPolls.has(msg.from.id)) {
             userPolls.set(msg.from.id, new Map());
         }
 
-        userPolls.get(msg.from.id).set(msg.message_id, wipPoll);
+        userPolls.get(msg.from.id).set(currPoll.id, currPoll);
 
-        bot.sendMessage(msg.from.id, JSON.stringify(wipPoll));
-        console.log(userPolls);
+        var reply = 'poll created.\n\n';
+        reply += '*' + currPoll.question + '*\n\n';
+        currPoll.choices.forEach(function(question){
+            reply += question + '\n';
+        })
+
+        opts = {
+            parse_mode: 'Markdown',
+            reply_markup: JSON.stringify({
+                inline_keyboard: [
+                    [
+                        {
+                            text: 'share me',
+                            switch_inline_query: currPoll.question
+                        }
+                    ],
+                    [
+                        {
+                            text: 'delete me',
+                            callback_data: '/delete ' + currPoll.id
+                        }
+                    ]
+                ]
+            })
+        }
+
+        bot.sendMessage(msg.from.id, reply, opts);
     } else {
         bot.sendMessage(msg.from.id, 'wtf u trying to do');
     }
@@ -66,15 +92,15 @@ bot.onText(/(.*)/, function(msg, match) {
         return;
     }
 
-    var wipPoll = wipPolls.get(msg.from.id);
-    if (wipPoll) {
-        if (!wipPoll.qn) {
-            wipPoll.qn = match[0];
+    var currPoll = currPolls.get(msg.from.id);
+    if (currPoll) {
+        if (!currPoll.question) {
+            currPoll.question = match[0];
         } else {
-            wipPoll.ans.push(match[0]);
+            currPoll.choices.push(match[0]);
         }
 
-        if (wipPoll.ans.length == 0) {
+        if (currPoll.choices.length == 0) {
             bot.sendMessage(msg.from.id, 'nice one lah. wat choice?');
         } else {
             bot.sendMessage(msg.from.id, 'nice one lah. wat other choice? /done if pang kang');
@@ -87,19 +113,35 @@ bot.on('inline_query', function(msg) {
 
     var polls = userPolls.get(msg.from.id);
     if (polls) {
-        polls.forEach(function(poll, id) {
-            results.push({
-                type: 'article',
-                id: id.toString(),
-                message_text: 'blahblah',
-                title: poll.qn,
-                description: poll.ans.toString()
-            });
+        polls.forEach(function(poll) {
+            if (poll.question.indexOf(msg.query) > -1) {
+                text = '*' + poll.question + '*\n\n';
+                poll.choices.forEach(function(question){
+                    text += question + '\n';
+                })
+
+                results.push({
+                    type: 'article',
+                    id: poll.id.toString(),
+                    message_text: text,
+                    parse_mode: 'Markdown',
+                    title: poll.question,
+                    description: poll.choices.toString()
+                });
+            }
         });
     }
     
     bot.answerInlineQuery(msg.id, results, {
+        cache_time: 0,
         switch_pm_text: 'new poll',
         is_personal: true
     });
-})
+});
+
+bot.on('callback_query', function(msg) {
+    if (msg.data.split(' ')[0] == '/delete') {
+        userPolls.get(msg.from.id).delete(parseInt(msg.data.split(' ')[1]));
+        bot.sendMessage(msg.from.id, "deleted liao");
+    }
+});
