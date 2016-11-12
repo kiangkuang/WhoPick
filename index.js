083b51d5fb4f4cb6af8587e85dad9942a39aef4d
Kiang Kuang <kiangkuang@gmail.com>
2016-11-13 02:23:03 +0800

    integrate db

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-require('dotenv').config({silent: process.env.NODE_ENV === 'production'});
-if (!process.env.BOT_TOKEN || process.env.BOT_TOKEN === '<token>') {
-    console.log('ERROR: "BOT_TOKEN" env variable not set.');
+require('dotenv').config({
+    silent: process.env.NODE_ENV === 'production'
+});
+if (!process.env.BOT_TOKEN || process.env.BOT_TOKEN === '<token>' || !process.env.DB_URL || process.env.DB_URL === '<db url>') {
+    console.log('ERROR: env variable not set.');
     return;
 }
 
 var TelegramBot = require('node-telegram-bot-api');
+var sprintf = require("sprintf-js").sprintf;
+var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.DB_URL);
 
 var token = process.env.BOT_TOKEN;
-// Setup polling way
 var bot = process.env.NODE_ENV === 'production' ?
-    new TelegramBot(token, {webHook: {port: process.env.PORT, host: '0.0.0.0'}}):
-    new TelegramBot(token, {polling: true});
+    new TelegramBot(token, {
+        webHook: {
+            port: process.env.PORT,
+            host: '0.0.0.0'
+        }
+    }) :
+    new TelegramBot(token, {
+        polling: true
+    });
 
 if (process.env.NODE_ENV === 'production') {
     bot.setWebHook('https://whopick.herokuapp.com/bot' + token);
@@ -23,60 +34,52 @@ bot.onText(/\/echo (.+)/, function(msg, match) {
 });
 
 var matched = false;
-var userPolls = new Map(); // polls of all users.       userPolls[userid][pollid] = poll
-var currPolls = new Map(); // currently being created.  currPolls[userid] = poll
-
+var questionMap = new Map(); // user_id : status
 
 // Matches /start
 bot.onText(/\/start/, function(msg, match) {
     matched = true;
-    poll = {
-        id: msg.message_id,
-        owner: msg.from.id,
-        question: '',
-        choices: []
-    };
-    currPolls.set(msg.from.id, poll);
-
+    questionMap.set(msg.from.id, -1);
     bot.sendMessage(msg.from.id, 'wat question?');
 });
 
 // Matches /done
 bot.onText(/\/done/, function(msg, match) {
     matched = true;
-    var currPoll = currPolls.get(msg.from.id);
-    if (currPoll && currPoll.question && currPoll.choices.length) { // currently creating a poll, have question, at least one choices
-        currPolls.delete(msg.from.id);
-        if (!userPolls.has(msg.from.id)) {
-            userPolls.set(msg.from.id, new Map());
-        }
 
-        userPolls.get(msg.from.id).set(currPoll.id, currPoll);
+    var questionId = questionMap.get(msg.from.id);
+    if (questionId > 0) { // currently creating a poll
+        connection.query('UPDATE question SET ? WHERE question_id = ?', [{
+            is_enabled: 1
+        }, questionId], function(err, result) {
+            if (err) throw err;
+            questionMap.delete(msg.from.id);
 
-        var reply = 'poll created.\n\n';
-        reply += formatPoll(poll);
+            var reply = 'poll created liao.\n\n';
 
-        opts = {
-            parse_mode: 'Markdown',
-            reply_markup: JSON.stringify({
-                inline_keyboard: [
-                    [
-                        {
-                            text: 'share me',
-                            switch_inline_query: currPoll.question
-                        }
-                    ],
-                    [
-                        {
-                            text: 'delete me',
-                            callback_data: '/delete ' + currPoll.id
-                        }
-                    ]
-                ]
-            })
-        }
+            connection.query('SELECT question.question_id, question, choice.choice_id, choice FROM question INNER JOIN choice ON question.question_id = choice.question_id LEFT JOIN vote ON choice.choice_id = vote.choice_id WHERE question.question_id = ?', questionId, function(err, result) {
+                if (err) throw err;
+                var polls = parseResult(result);
+                var poll = polls[questionId];
+                opts = {
+                    parse_mode: 'Markdown',
+                    reply_markup: JSON.stringify({
+                        inline_keyboard: [
+                            [{
+                                text: 'share me',
+                                switch_inline_query: poll.question
+                            }],
+                            [{
+                                text: 'delete me',
+                                callback_data: '/delete ' + poll.question_id
+                            }]
+                        ]
+                    })
+                }
 
-        bot.sendMessage(msg.from.id, reply, opts);
+                bot.sendMessage(msg.from.id, formatPoll(poll), opts);
+            });
+        });
     } else {
         bot.sendMessage(msg.from.id, 'wtf u trying to do');
     }
@@ -89,88 +92,156 @@ bot.onText(/(.*)/, function(msg, match) {
         return;
     }
 
-    var currPoll = currPolls.get(msg.from.id);
-    if (currPoll) {
-        if (!currPoll.question) {
-            currPoll.question = match[0];
-        } else {
-            currPoll.choices.push(
-                {
-                    text: match[0],
-                    voters: []
-                }
-            );
-        }
-
-        if (currPoll.choices.length == 0) {
+    var questionId = questionMap.get(msg.from.id);
+    if (questionId == -1) {
+        connection.query('INSERT INTO question SET ?', {
+            user_id: msg.from.id,
+            question: match[0]
+        }, function(err, result) {
+            if (err) throw err;
+            questionMap.set(msg.from.id, result.insertId);
             bot.sendMessage(msg.from.id, 'nice one lah. wat first choice?');
-        } else {
-            bot.sendMessage(msg.from.id, 'nice one lah. wat other choice?\n/done if pang kang');
-        }
+        });
+    } else if (questionId > 0) {
+        connection.query('INSERT INTO choice SET ?', {
+            question_id: questionId,
+            choice: match[0]
+        }, function(err, result) {
+            if (err) throw err;
+            bot.sendMessage(msg.from.id, 'nice one lah. wat other choice?\n/done if pang kang.');
+        });
     }
 });
 
 bot.on('inline_query', function(msg) {
-    var polls = userPolls.get(msg.from.id);
-    if (polls) {
-        var results = [];
-
-        polls.forEach(function(poll) {
-            if (poll.question.indexOf(msg.query) > -1) {
-                var inlineKeyboard = poll.choices.map(function(choice, i) {
-                    return [{text: choice.text, callback_data: '/vote ' + msg.from.id + ' ' + poll.id + ' ' + i}];
-                });
-                var description = poll.choices.map(function(choice) {
-                    return choice.text;
-                });
-
-                results.push({
-                    type: 'article',
-                    id: poll.id.toString(),
-                    message_text: formatPoll(poll),
-                    parse_mode: 'Markdown',
-                    title: poll.question,
-                    description: description.toString(),
-                    reply_markup: {
-                        inline_keyboard: inlineKeyboard
-                    }
-                });
-            }
+    connection.query('SELECT q.question_id, question, c.choice_id, choice, v.vote_id, v.user_id, v.name FROM question q INNER JOIN choice c ON q.question_id = c.question_id LEFT JOIN vote v ON c.choice_id = v.choice_id WHERE q.user_id = ? AND question LIKE ?', [msg.from.id, '%' + msg.query + '%'], function(err, result) {
+        if (err) throw err;
+        var polls = parseResult(result);
+        var reply = [];
+        polls.map(function(poll) {
+            reply.push({
+                type: 'article',
+                id: poll.question_id.toString(),
+                message_text: formatPoll(poll),
+                parse_mode: 'Markdown',
+                title: poll.question,
+                description: getDescription(poll),
+                reply_markup: {
+                    inline_keyboard: getInlineKeyboard(poll)
+                }
+            });
         });
-    }
-    
-    bot.answerInlineQuery(msg.id, results, {
-        cache_time: 0,
-        switch_pm_text: 'new poll',
-        is_personal: true
+
+        bot.answerInlineQuery(msg.id, reply, {
+            cache_time: 0,
+            switch_pm_text: 'new poll',
+            is_personal: true
+        });
     });
 });
 
 bot.on('callback_query', function(msg) {
     var commands = msg.data.split(' ');
     switch (commands[0]) {
-        case '/delete':
-            userPolls.get(msg.from.id).delete(parseInt(commands[1]));
-            bot.sendMessage(msg.from.id, "deleted liao");
+        case '/delete': // /delete question_id
+            connection.query('DELETE FROM question WHERE question_id = ?', commands[1], function(err, result) {
+                if (err) throw err;
+                bot.editMessageText('deleted liao', {
+                    chat_id: msg.message.chat.id,
+                    message_id: msg.message.message_id
+                });
+            });
             break;
-        case '/vote': // /vote userid pollid choiceIndex
-            var poll = userPolls.get(parseInt(commands[1])).get(parseInt(commands[2]));
-            poll.choices[commands[3]].voters.push(msg.from.first_name);
-            console.log(poll)
-            console.log(poll.choices)
+        case '/vote': // /vote question_id choice_id
+            connection.query('INSERT INTO vote SET ?', {
+                choice_id: commands[2],
+                user_id: msg.from.id,
+                name: msg.from.first_name
+            }, function(err, result) {
+                if (err) return;
+                connection.query('SELECT q.question_id, question, c.choice_id, choice, v.vote_id, v.user_id, v.name FROM question q INNER JOIN choice c ON q.question_id = c.question_id LEFT JOIN vote v ON c.choice_id = v.choice_id WHERE q.question_id = ?', commands[1], function(err, result) {
+                    if (err) throw err;
+                    var polls = parseResult(result);
+                    var poll = polls[commands[1]];
+                    var opts = {
+                        inline_message_id: msg.inline_message_id,
+                        parse_mode: 'Markdown',
+                        reply_markup: {
+                            inline_keyboard: getInlineKeyboard(poll)
+                        }
+                    };
+                    bot.editMessageText(formatPoll(poll), opts);
+                });
+            });
             break;
     }
 });
 
-function formatPoll(poll) {
-    text = '*' + poll.question + '*\n';
-    poll.choices.forEach(function(choice) {
-        text += '_' + choice.text + '_\n';
+function parseResult(result) {
+    var polls = [];
+    result.forEach(function(row) {
+        if (polls[row.question_id] == null) {
+            // create new question
+            polls[row.question_id] = {
+                question_id: row.question_id,
+                question: row.question,
+                choices: []
+            };
+        }
+        var question = polls[row.question_id];
 
-        choice.voters.forEach(function(voter, i) {
-            text += (i + 1) + ') ' + voter + '\n';
+        if (question.choices[row.choice_id] == null) {
+            // create new choice
+            question.choices[row.choice_id] = {
+                choice_id: row.choice_id,
+                choice: row.choice,
+                votes: []
+            };
+        }
+        var choice = question.choices[row.choice_id];
+
+        if (choice.votes[row.vote_id] == null) {
+            // create new vote
+            choice.votes[row.vote_id] = {
+                vote_id: row.vote_id,
+                user_id: row.user_id,
+                name: row.name
+            };
+        }
+    });
+    return polls;
+}
+
+function formatPoll(poll) {
+    result = sprintf('*%s*\n', poll.question);
+
+    poll.choices.forEach(function(choice) {
+        result += sprintf('_%s_\n', choice.choice);
+
+        var counter = 1;
+        choice.votes.forEach(function(vote) {
+            result += sprintf('%d) %s\n', counter++, vote.name)
         });
     });
+    return result;
+}
 
-    return text;
+function getInlineKeyboard(poll) {
+    var result = [];
+    poll.choices.forEach(function(choice, i) {
+        // /vote question_id choice_id
+        result.push([{
+            text: choice.choice,
+            callback_data: '/vote ' + poll.question_id + ' ' + choice.choice_id
+        }]);
+    });
+    return result;
+}
+
+function getDescription(poll) {
+    var result = [];
+    poll.choices.forEach(function(choice) {
+        result.push(choice.choice);
+    });
+    return result.join(', ');
 }
