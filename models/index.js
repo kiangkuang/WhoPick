b2848c83ea8e2bf01230ceb866b08e12dbca6424
Kiang Kuang <kiangkuang@gmail.com>
2016-12-09 02:15:38 +0800

    remove config.json

--- models/index.js
+++ models/index.js
@@ -4,15 +4,9 @@ var fs = require('fs');
 var path = require('path');
 var Sequelize = require('sequelize');
 var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || 'development';
-var config = require(__dirname + '/../config/config.json')[env];
 var db = {};
 
-if (config.use_env_variable) {
-    var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-    var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+var sequelize = new Sequelize(process.env.DB_URL);
 
 fs
     .readdirSync(__dirname)
