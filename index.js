971c2e0080905333b2a45bc4f741b44cfcafc5f5
Kuek Kiang Kuang <kiangkuang@gmail.com>
2017-02-23 11:48:19 +0800

    add switch_pm_parameter

--- index.js
+++ index.js
@@ -212,6 +212,7 @@ function inlineQuery(queryId, userId, query) {
         bot.answerInlineQuery(queryId, reply, {
             cache_time: 0,
             switch_pm_text: 'Create new poll',
+            switch_pm_parameter: '',
             is_personal: true
         });
     });
