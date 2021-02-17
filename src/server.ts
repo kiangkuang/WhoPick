355178f115904868272c107b5286c3c3acd126de
Kiang Kuang <kiangkuang@gmail.com>
2021-02-18 01:13:35 +0800

    telegraf

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,31 @@
-import { getQuestion } from './repository';
+import express from 'express';
+import { Telegraf } from 'telegraf';
+import Debug from 'debug';
 
-(async () => {
-  try {
-    const a = await getQuestion(58);
-    console.log(JSON.stringify(a, null, 2));
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-})();
+const debug = Debug('whopick');
+
+const token = process.env.BOT_TOKEN;
+if (token === undefined) {
+  throw new Error('BOT_TOKEN must be provided!');
+}
+
+const bot = new Telegraf(token);
+// Set the bot response
+bot.on('text', (ctx) => ctx.replyWithHTML('<b>Hello</b>'));
+
+// Set telegram webhook
+if (process.env.NODE_ENV === 'local') {
+  bot.launch();
+} else {
+  bot.telegram.setWebhook(`https://${process.env.HEROKU_APP_NAME}.herokuapp.com/bot`);
+}
+
+const app = express();
+app.get('/', (req, res) => res.send('Hello World!'));
+// Set the bot API endpoint
+app.use(bot.webhookCallback('/bot'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  debug(`Listening on port ${port}`);
+});
